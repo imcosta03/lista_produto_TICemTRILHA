39aa71daacfb38c625b3f2986da65fe7c4b98fc4
imcosta03 <costa.repositorio@gmail.com>
2025-08-16 21:20:15 -0400

    Desenvolvimento das seguintes funções remover_produto | mostrar_produto | pesquisar_produto | exibir_menu | escolher_menu

--- app.py
+++ app.py
@@ -53,5 +53,61 @@ def adicionar_produto():
     print(f"\nProduto '{nome}' adicionado com sucesso com ID = {produto_id}\n")
     produto_id += 1
 
-adicionar_produto()
+def remover_produto():
+    try:
+        remover_id = int(input("Digite o ID para remover o produto: "))
+    except ValueError:
+        print("ID inválido, tente novamente!")
+        return
+    
+    for produto in lista_compras:
+        if produto["id"] == remover_id:
+            lista_compras.remove(produto)
+            print(f"ID do produto não encontrado")
+
+def mostrar_produto():
+    if not lista_compras:
+        print("Lista zerada, Digite A para adicionar")
+    else:
+        print(f"{'ID':<5} {'Nome':<15} {'Unid':<6} {'Qtd'}")
+        print("-"*42)
+        for produto in lista_compras:
+            print(f"{produto['id']:<5} {produto['nome']:<15} {produto['unidade']:<6} {produto['quantidade']}")
+
+def pesquisar_produto():
+    nome_produto = input("Qual produto você está procurando: ").lower()
+    encontrados = [p for p in lista_compras if nome_produto in p["nome"].lower()]
+    print(f"\n {len(encontrados)} produto(s) encontrado(s): ")
+    
+    for p in encontrados:
+        print(f"ID {p['id']} - {p['nome']} {p['quantidade']}  {p['unidade']}  {p['descricao']}")
+
+def exibir_menu():
+    print(f"\n{'*' *7} Programa Lista de Produtos {'*' *7} ") 
+    print("""
+        [A] Adicionar       [R] Remover
+        [P] Pesquisar       [S] Sair
+    """)
+    print('-'*42)
+
+def escolher_menu():
+    while True:
+        mostrar_produto()
+        exibir_menu()
+        opcao = input("Escolha uma opção: ").upper()
+
+        if opcao == "A":
+            adicionar_produto()
+        elif opcao == "R":
+            mostrar_produto()
+            remover_produto()
+        elif opcao == "P":
+            pesquisar_produto()
+            continue
+        elif opcao == "S":
+            print("Agradecemos por utilizar nosso serviço, volte sempre!")
+            break
+        else: 
+            print("Opção invalida")
+
 
